721544a89d540f1cdd3af77605a63b06b9c14b92
Ankita27052002 <manadalankita2020@gmail.com>
2025-09-19 19:18:05 +0530

    React Router folder added

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import UserData from "./custom-hooks/userData";
 import WithLiftingStateUp from "./liftingStateUp/WithLiftingStateUp";
 import WithoutLiftingStateUp from "./liftingStateUp/WithoutLiftingStateUp";
@@ -5,6 +6,8 @@ import Dashboard from "./propsDrilling/components/Dashboard";
 import FilterUserWithCallback from "./useCallback/FilterUserWithCallback";
 import FilterUser from "./useMemo/FilterUser";
 import PreviousCounter from "./useRef/PreviousCounter";
+import Home from "./react-router/Home";
+import About from "./react-router/About";
 
 
 const App = () => {
@@ -27,8 +30,23 @@ const App = () => {
 
       {/* for props drilling example */}
       <Dashboard name={userName}/>
+
+      {/* for Browser router */}
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link>
+        </nav>
+
+
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 };
 
 export default App;
---
+++ Hooks/src/react-router/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home sweet Home</h2>
+    </div>
+  );
+}
+
+export default Home;
